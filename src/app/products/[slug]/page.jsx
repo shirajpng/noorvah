8526ab9c6cf88e39231a3759cf6ac1143956a59c
shiraj pradhanang <shirajpng@gmail.com>
2025-04-03 13:21:23 +0545

    error fixes

--- src/app/products/[slug]/page.jsx
+++ src/app/products/[slug]/page.jsx
@@ -1,14 +1,10 @@
-import { getPosts } from '@/lib/posts'
-import { Post } from '@/ui/post'
+
  
 export default async function Page() {
-  const posts = await getPosts()
- 
+  
   return (
-    <ul>
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
-    </ul>
+    <>
+      
+    </>
   )
 }
---
+++ src/app/products/page.jsx
@@ -0,0 +1,20 @@
+import { products } from "config/products.json"
+
+export default function Page() {
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-10 ">
+            {products.filter((product) => product.is_featured == true).map((product, index) => (
+                <div key={index} className="bg-white rounded-lg shadow-md p-4">
+                    <img
+                        src={product.image}
+                        alt={product.name}
+                        className="w-full h-42 object-cover rounded-md mb-4"
+                    />
+                    <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+                    <p className="text-gray-600 mb-2">Description of product {index + 1}.</p>
+                    <p className="text-xl font-bold">Rs {product.price}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
