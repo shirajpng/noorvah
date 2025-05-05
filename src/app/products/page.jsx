5ade76c6527da1604b17dfda95dab8fc5e991370
shiraj pradhanang <shirajpng@gmail.com>
2025-05-05 17:48:21 +0545

    title in products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,19 +2,24 @@ import data from "config/products.json"
 
 export default function Page() {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-10 mb-6">
-            {data.products.filter((product) => product.is_featured == true).map((product, index) => (
-                <div key={index} className="bg-white rounded-lg shadow-md p-4">
-                    <img
-                        src={product.image}
-                        alt={product.name}
-                        className="w-full h-42 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-                    <p className="text-gray-600 mb-2">Description of product {index + 1}.</p>
-                    {/* <p className="text-xl font-bold">Rs {product.price}</p> */}
-                </div>
-            ))}
-        </div>
+        <>
+            <span className="text-[40px] flex flex-col p-4 items-center">
+                Our Products
+            </span>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-10 mb-6">
+                {data.products.filter((product) => product.is_featured == true).map((product, index) => (
+                    <div key={index} className="bg-white rounded-lg shadow-md p-4">
+                        <img
+                            src={product.image}
+                            alt={product.name}
+                            className="w-full h-42 object-cover rounded-md mb-4"
+                        />
+                        <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+                        <p className="text-gray-600 mb-2">Description of product {index + 1}.</p>
+                        {/* <p className="text-xl font-bold">Rs {product.price}</p> */}
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
