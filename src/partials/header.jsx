dc46e4aa1f7a8edce26a0d8f96e535e71ee19e73
shiraj pradhanang <shirajpng@gmail.com>
2025-05-05 17:53:31 +0545

    header color fix

--- src/partials/header.jsx
+++ src/partials/header.jsx
@@ -15,29 +15,21 @@ const Header = () => {
                     {/* <span className="text-[30pt] font-light hidden md:block">NOORVAH</span> */}
                 </div>
                 <nav>
-                <style jsx>{`
-                    .font-blue-custom {
-                    color: #309FED;
-                    }
-                    p {
-                    color: blue;
-                    }
-                `}</style>
                     <ul className="flex gap-x-4">
                         <li>
-                            <Link href="/" className={`${pathname === "/" && 'font-blue-custom'}`}>Home</Link>
+                            <Link href="/" className={`${pathname === "/" && 'text-[#309FED]'}`}>Home</Link>
                         </li>
                         <li>|</li>
                         <li>
-                            <Link href="/about-us" className={`${pathname.includes("/about-us") && 'font-blue-custom'}`}>About</Link>
+                            <Link href="/about-us" className={`${pathname.includes("/about-us") && 'text-[#309FED]'}`}>About</Link>
                         </li>
                         <li>|</li>
                         <li>
-                            <Link href="/products" className={`${pathname.includes("/products") && 'font-blue-custom'}`}>Products</Link>
+                            <Link href="/products" className={`${pathname.includes("/products") && 'text-[#309FED]'}`}>Products</Link>
                         </li>
                         <li>|</li>
                         <li>
-                            <Link href="/contact" className={`${pathname.includes("/contact") && 'font-blue-custom'}`}>Contact</Link>
+                            <Link href="/contact" className={`${pathname.includes("/contact") && 'text-[#309FED]'}`}>Contact</Link>
                         </li>
                     </ul>
                 </nav>
